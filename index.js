70f1ae0d2e465f75694dd17d2c304f49c7adb0f4
Neciu Dan <dan@adoreme.com>
2019-10-15 08:47:23 -0400

    changed the video recording

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const cron = require("node-cron");
 let shell = require("shelljs");
 
-cron.schedule("0 30 9-19 * * *", function() {
+cron.schedule("0 30 8-19 * * *", function() {
     console.log("---------------------");
     console.log("Running Cron Job");
-    if (shell.exec("npm run cy:run -- --spec 'cypress/integration/change_waiver_hour.js'").code !== 0) {
+    if (shell.exec("npm run cy:run --spec 'cypress/integration/change_waiver_hour.js'").code !== 0) {
         shell.exit(1);
     }
     else{
